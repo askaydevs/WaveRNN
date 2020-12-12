652b0790c7bc153a4a27d8902950dcbcfc464baa
Shubham Singh <sk_singh18@outlook.com>
2020-12-12 22:31:58 +0530

    Update dsp.py
    Replaced `soundfile.write` for saving wav

--- utils/dsp.py
+++ utils/dsp.py
@@ -20,7 +20,7 @@ def load_wav(path):
 
 
 def save_wav(x, path):
-    librosa.output.write_wav(path, x.astype(np.float32), sr=hp.sample_rate)
+    librosa.soundfile.write(path, x.astype(np.float32), sr=hp.sample_rate)
 
 
 def split_signal(x):
